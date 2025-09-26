01de5a543edf67533f1daafba7ea3480d468f506
Fabricio Roque <fabriciobroque@gmail.com>
2025-09-26 09:09:33 -0300

    Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import Services from "@/sections/Services";
 import About from "@/sections/About";
 import Footer from "@/components/Footer";
 import GoogleAnalytics from "./GoogleAnalytics";
+import { Analytics } from '@vercel/analytics/next';
 
 
 function App() {
 return (
 <div className="font-sans text-gray-800">
-    <GoogleAnalytics />
+<GoogleAnalytics />
 <Header />
 <Hero />
 <Services />
 <About />
 <Footer />
+<Analytics />
 </div>
 );
 }
